f00c39470fbc95d883a6be335010bc78de693a68
22 <22@DESKTOP-E2OU7HF>
2021-04-13 09:14:20 +0900

    프린트문 수정

--- Test20210413/src/first/FirstClass.java
+++ Test20210413/src/first/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
-		System.out.println("첫번째 클래스");
+		System.out.println("지우고 다시 씀");
 
 	}
 
